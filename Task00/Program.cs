7edaa5598094520c37be0db719eace9e67d2bedb
Tarantin <mr.fadeyka@gmail.com>
2023-04-03 09:27:35 +0300

    added task00

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая на вход принимает число, а на выход выводит его квадрат
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+//или string str = Console.ReadLine();
+//    int number = Convert.ToInt32(str);
+int square = number * number;
+Console.WriteLine($"Квадрат числа {number} = {square}");
