7ae3b2326b852297ea4831843bdc4fce4b8c1b46
Tarantin <mr.fadeyka@gmail.com>
2023-04-03 17:49:22 +0300

    Решили Таск01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу которая на вход пренимает два числа и определяет евляется ли первое число 
+// квадратом второго.
+// а = 25, b = 5 -> yes
+// а = 2, b = 10 -> no
+// а = 9, b = -3 -> yes
+// а = -3, b = 9 -> no
+Console.Write("Введите первое число -> ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число -> ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber % secondNumber == 0)
+{
+  Console.WriteLine($"Да число {firstNumber} является квадратом числа {secondNumber}");
+}
+
+else
+{
+  Console.WriteLine($"Нет число {firstNumber} не является квадратом числа {secondNumber}");
+}
